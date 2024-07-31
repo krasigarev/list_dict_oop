c52c4493d0fd6583d84c33cc728995a4f97aa1c2
krasigarev <kgarev@gmail.com>
2024-07-31 14:16:24 +0300

    data list append

--- list_1.py
+++ list_1.py
@@ -1,30 +1,36 @@
-# temperature = [20, 21, 20, 25, "Hi"]
-#
-# for temp in temperature:
-#     print(temp)
-#
-# for index in range(len(temperature)):
-#     print(temperature[index])
+temperature = [20, 21, 20, 25, "Hi"]
+
+for temp in temperature:
+    print(temp)
+
+for index in range(len(temperature)):
+    print(temperature[index])
 
 
 # Multiply a List of Integers version 1
 
-# data_list = input().split(" ")
-# p = int(input())
-#
-# numbers_list = []
-#
-# for el in data_list:
-#     numbers_list.append(int(el) * p)
-#
-# print(data_list)
-# print(numbers_list)
+data_list = input().split(" ")
+p = int(input())
+
+numbers_list = []
+
+for el in data_list:
+    numbers_list.append(int(el) * p)
+
+print(data_list)
+print(numbers_list)
 
 
 # Multiply a List of Integers version 2
 
+def multiply(el , p):
+    return el * p
+
+
 data_list_1 = list(map(int, input().split(" ")))
 p = int(input())
 
-a = 4 * data_list_1
-print(a)
+a = [multiply(element, p) for element in data_list_1]
+
+print(*a)
+print(" ".join(map(str, a)))
