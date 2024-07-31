9385b1b845c1925918be618f444236706e08dff0
krasigarev <kgarev@gmail.com>
2024-07-31 16:56:03 +0300

    data list sort and sorted

--- smallest item in list.py
+++ smallest item in list.py
@@ -3,3 +3,24 @@
 num = list(map(int, input().split(" ")))
 
 print(min(num))
+
+min_number = num[0]
+
+for element in num:
+    if element < min_number:
+        min_number = element
+
+print(min_number)
+
+data_min = sorted(num)
+
+print(data_min)
+
+num.sort()
+
+print(num)
+
+sorted_num = sorted(num)
+
+print(sorted_num)
+print(num)
