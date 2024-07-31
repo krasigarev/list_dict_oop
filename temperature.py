79b01ffb51fbd1b16d38f17171521e2b604b8291
krasigarev <kgarev@gmail.com>
2024-07-31 19:07:23 +0300

    append and extend list python

---
+++ temperature.py
@@ -0,0 +1,11 @@
+temperature = [20, 21, 20]
+
+other_temperature = [19, 25]
+
+temperature.append(other_temperature)
+
+print(temperature)
+
+temperature.extend(other_temperature)
+
+print(temperature)
