f23b93ce75a562c5e1d29b2941a684ecfec3ba8c
krasigarev <kgarev@gmail.com>
2024-07-31 10:40:34 +0300

    compare values

--- compare_str_int_float.py
+++ compare_str_int_float.py
@@ -1,2 +1,14 @@
 # ascii table
 
+def compare_values(value_1, value_2):
+    if value_1 < value_2:
+        return value_2
+    return value_1
+
+
+if __name__ == "__main__":
+    value_type = input()
+    value_1 = input()
+    value_2 = input()
+
+    print(compare_values(value_1, value_2))
