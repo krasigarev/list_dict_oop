77fa9013680940d19e1b1eec583a836b987dbb66
krasigarev <kgarev@gmail.com>
2024-07-31 13:54:32 +0300

    compare values

--- list_1.py
+++ list_1.py
@@ -1,7 +1,21 @@
-temperature = [20, 21, 20, 25, "Hi"]
+# temperature = [20, 21, 20, 25, "Hi"]
+#
+# for temp in temperature:
+#     print(temp)
+#
+# for index in range(len(temperature)):
+#     print(temperature[index])
 
-for temp in temperature:
-    print(temp)
 
-for index in range(len(temperature)):
-    print(temperature[index])
+# Multiply a List of Integers
+
+data_list = input().split(" ")
+p = int(input())
+
+numbers_list = []
+
+for el in data_list:
+    numbers_list.append(int(el) * p)
+
+print(data_list)
+print(numbers_list)
