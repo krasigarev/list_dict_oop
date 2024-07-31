49e8ed100d66abfdebf7c4caae563cff53f3df41
krasigarev <kgarev@gmail.com>
2024-07-31 17:12:14 +0300

    data list sort and sorted

--- smallest item in list.py
+++ smallest item in list.py
@@ -24,3 +24,21 @@ sorted_num = sorted(num)
 
 print(sorted_num)
 print(num)
+
+list_2 = ["one", "two", "three", "four", "five", "six"]
+
+for i in list_2:
+    print(i)
+
+print("; ".join(list_2))
+
+values = input()
+items = values.split(' ')
+nums_1 = []
+for i in items:
+    nums_1 += [int(i)]   # or nums.append(int(i))
+
+print(nums_1)
+
+num_2 = [int(item) for item in input(" ").split()]
+print(num_2)
